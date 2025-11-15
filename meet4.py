ab449a0bcd76f2670e25edd1f2e02c856dcd17bc
lalunofrizalfirmansyah_Alprog-C <firmansayhh05112006@gmail.com>
2025-11-15 19:31:05 +0700

    Add files via upload

---
+++ meet4.py
@@ -0,0 +1,21 @@
+# mengambil input data dari user 
+
+nama = input ("masukkan nama anda ")
+print("SELAMAT datang ", nama)
+print("nilai dari nama", nama)
+print(type(nama))
+
+bil_int = int(input("minputasukkan bilangan anda "))
+print("bilangan anda adalah ", bil_int)
+print("nilai dari bil_int ", bil_int)
+print(type(bil_int))
+
+bil_float = float(input("masukkan bilangan anda "))
+print("bilangan anda adalah ", bil_float)
+print("nilai dari bilangan bil_float", bil_float)
+print(type(bil_float))
+
+bil_bool = bool(input("masukkan bilangan anda "))
+print("bilangan anda adlah bil_bool ", bil_bool)
+print("nilai dari bil_bool ", bil_bool)
+print(type(bil_bool))
