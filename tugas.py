89626cb5f9c8e8d9eb83e62e944d64b9689c7e3b
lalunofrizalfirmansyah_Alprog-C <firmansayhh05112006@gmail.com>
2025-11-15 20:32:21 +0700

    Add files via upload

---
+++ tugas.py
@@ -0,0 +1,15 @@
+# output untuk nama
+
+nama = input("masukkan nama anda ")
+print("selamat datang ", nama)
+print("tipe data dari nama anda adalah", nama)
+print(type(nama))
+
+#output untuk umur
+
+umur = int(input("masukkan umur anda "))
+print("umur anda adalah", umur)
+print("tipe data dari umur anda adalah", umur)
+print(type(umur))
+
+
