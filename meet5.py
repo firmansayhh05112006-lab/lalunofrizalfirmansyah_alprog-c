8d6d2ead37feb8b520844a07de15c4edb0a3160c
lalunofrizalfirmansyah_Alprog-C <firmansayhh05112006@gmail.com>
2025-11-29 18:28:02 +0700

    Add files via upload

---
+++ meet5.py
@@ -0,0 +1,28 @@
+# operasi aritmatika (x : + -)
+# penjumlahan
+a = 7
+b=3
+hasil = a + b 
+print(a, "+", b, "=", hasil)
+
+# pengurangan -
+# perkalian *
+# pembagian /
+hasil = a / b 
+print(a, "/", b, "=", hasil)
+# perpangkatan **
+
+#modulus (sisa bagi)
+hasil = a % b 
+print(a, "modulus", b, "=", hasil)
+
+# flor davision // (pembulatan)
+hasil = a // b
+print(a, "floor div", b, "=", hasil)
+
+# operasi prioritas
+# 1. ()
+# 2. pangkat akar 
+# 3. x : 
+# 4. + -
+
