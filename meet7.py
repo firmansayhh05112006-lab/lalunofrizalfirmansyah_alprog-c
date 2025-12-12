9005f20d4a1c334bf4f599957fe051f4004ccd29
lalunofrizalfirmansyah_Alprog-C <firmansayhh05112006@gmail.com>
2025-12-12 21:50:56 +0800

    Add files via upload

---
+++ meet7.py
@@ -0,0 +1,75 @@
+# kemperasi (true, false)
+# > < >= <= == != is is-not 
+# nama: lalu nofrizal firmansyah 
+# nim: 25241080
+# x = (2 digit nim pertama)
+# y (2 digital nim akhir)
+x = 6
+y = 5
+hasil = x > 5 
+print(x, ">", 5, "=", hasil)
+
+hasil = y < 6
+print(y, "<", 6, "=", hasil)
+
+hasil = x >= 5
+print(x, ">=", 5, "=", hasil)
+
+hasil = y >= 6
+print(y, ">=", 6, "=", hasil)
+
+hasil = y == 6
+print(y, "==", 6, "=", hasil)
+
+hasil = y != 6
+print(y, "!=", 6, "=", hasil)
+
+hasil = y is not 6
+print(y, "is not", 6, "=", hasil)
+
+# TUGAS 
+x = 25 
+y = 76
+hasil = x > 76
+print(x, ">", 76, "=", hasil)
+hasil = x > 24
+print(x, ">", 76, "=", hasil)
+
+hasil = y < 25
+print(x, "<", 25, "=", hasil)
+hasil = x < 76
+print(x, "<", 76, "=", hasil)
+
+hasil = y >= 76
+print(x, ">=", 76, "=", hasil)
+hasil = x >= 25
+print(x, ">=", 25, "=", hasil)
+
+hasil = y <= 76
+print(x, "<=", 76, "=", hasil)
+hasil = x <= 25
+print(x, "<=", 25, "=", hasil)
+
+hasil = y == 76
+print(x, "==", 76, "=", hasil)
+hasil = x == 25
+print(x, "==", 25, "=", hasil)
+
+hasil = y != 76
+print(x, "!=", 76, "=", hasil)
+hasil = x != 25
+print(x, "!=", 25, "=", hasil)
+
+hasil = y is 76
+print(x, "is", 76, "=", hasil)
+hasil = x is 25
+print(x, "is", 25, "=", hasil)
+
+hasil = y is not 76
+print(x, "is not", 76, "=", hasil)
+hasil = x is not 25
+print(x, "is not", 25, "=", hasil)
+
+
+
+
