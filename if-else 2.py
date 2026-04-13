d522be036c18b33b560fa02e6560648501a59f22
lalunofrizalfirmansyah_Alprog-C <firmansayhh05112006@gmail.com>
2026-04-13 15:09:43 +0800

    Add files via upload

---
+++ if-else 2.py
@@ -0,0 +1,8 @@
+nama = input("masukan nama anda:")
+if nama=="nofrizal" :
+   print("selamat datang novrizal")
+   print("nim anda 25241080")
+else :
+   print("anda bukan dosen/mahasiswa")
+   print("anda bukan nofrizal")
+print("program selesai")
