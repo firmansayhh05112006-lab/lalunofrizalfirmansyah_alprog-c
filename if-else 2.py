6c89d8bf3726bac48c2cdd1e8c62eff9184a9b3f
lalunofrizalfirmansyah_Alprog-C <firmansayhh05112006@gmail.com>
2026-04-13 16:08:31 +0800

    Delete if-else 2.py

--- if-else 2.py
+++
@@ -1,8 +0,0 @@
-nama = input("masukan nama anda:")
-if nama=="nofrizal" :
-   print("selamat datang novrizal")
-   print("nim anda 25241080")
-else :
-   print("anda bukan dosen/mahasiswa")
-   print("anda bukan nofrizal")
-print("program selesai")
